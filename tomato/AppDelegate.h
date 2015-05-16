87732f6144a48f4effe5dbec68c3f616be0d9416
Julie Lee <julielee@Julie-no-MacBook-Pro.local>
2015-05-16 15:58:47 +0900

    Initial Commit

---
+++ tomato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tomato
+//
+//  Created by Julie Lee on 2015/05/16.
+//  Copyright (c) 2015年 JulieLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tomato/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tomato
+//
+//  Created by Julie Lee on 2015/05/16.
+//  Copyright (c) 2015年 JulieLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
